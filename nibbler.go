652e22e3755b12d0c4d9fa6f8bd567eaf0bf973c
Andrew Taggart <andrew.taggart@sendgrid.com>
2015-04-22 19:07:13 -0600

    Initial commit

---
+++ nibbler.go
@@ -0,0 +1,111 @@
+package go_nibbler
+
+import (
+	"strings"
+)
+
+type state struct {
+	InQuotes, PreviousDot, PreviousSlash, InDomain bool
+}
+
+const ()
+
+const (
+	ATEXT    = "!#$%&'*+-/=?^_`.{|}~@\"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	SPECIAL  = "(),:;<>[\\] "
+	HOSTNAME = "-.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+func ParseEmail(email string) (bool, string) {
+	valid := true
+	var address string
+	currentState := state{}
+
+	for offset, character := range email {
+
+		// Local part
+		if !currentState.InDomain {
+			if character == '\\' {
+				if currentState.InQuotes {
+					// Check if slash was backslashed within quotes
+					if currentState.PreviousSlash {
+						currentState.PreviousSlash = false
+					} else {
+						currentState.PreviousSlash = true
+					}
+					// \ can only occur within slashes
+				} else {
+					valid = false
+					break
+				}
+			} else if character == '"' {
+
+				if currentState.InQuotes {
+					// Ignore if it was preceded by a backslash
+					if !currentState.PreviousSlash {
+						currentState.InQuotes = false
+					} else {
+						//                     else:
+						currentState.PreviousSlash = false
+					}
+				} else {
+					// Quotes must happen as the first character or after a dot
+					if offset != 0 && !currentState.PreviousDot {
+						valid = false
+						break
+					} else {
+						currentState.InQuotes = true
+					}
+				}
+			} else if character == '.' {
+				// We can't have two consecutive dots
+				if !currentState.InQuotes {
+					currentState.PreviousDot = true
+				}
+			} else if character == '@' && !currentState.InQuotes {
+				currentState.InDomain = true
+			}
+			if strings.ContainsRune(SPECIAL, character) {
+				// These characters must only occur in quotes
+				if strings.ContainsRune(SPECIAL, character) {
+					if !currentState.InQuotes {
+						valid = false
+						break
+					} else {
+						address += string(character)
+					}
+				}
+			} else {
+
+				if !strings.ContainsRune(ATEXT, character) {
+					valid = false
+					break
+				} else {
+					address += string(character)
+				}
+			}
+			// Check states and clear them if necessary
+			if currentState.PreviousSlash && character != '\\' {
+				currentState.PreviousSlash = false
+			}
+			if currentState.PreviousDot && character != '.' {
+				currentState.PreviousDot = false
+			}
+
+			// Domain part, we don't allow ip addresses [x.x.x.x] only host names
+		} else {
+
+			if !strings.ContainsRune(HOSTNAME, character) {
+				valid = false
+				break
+			} else {
+				address += string(character)
+			}
+		}
+	}
+	if !currentState.InDomain {
+		valid = false
+	}
+	return valid, address
+
+}
---
+++ nibbler_test.go
@@ -0,0 +1,26 @@
+package go_nibbler
+
+import "testing"
+
+type example struct {
+	Input   string
+	Valid   bool
+	Address string
+}
+
+var examples = []example{
+	example{"woo", false, "woo"},
+	example{"woo@woot.com", true, "woo@woot.com"},
+	example{"woo/+@blah.com", true, "woo/+@blah.com"},
+	example{"#!$%&'*+-/=?^_`{}|~@example.org", true, "#!$%&'*+-/=?^_`{}|~@example.org"},
+	example{"\"Bob\" <bobthebuilder@dlc.com>", false, "\"Bob\""},
+}
+
+func TestNibbler(t *testing.T) {
+	for _, e := range examples {
+		valid, address := ParseEmail(e.Input)
+		if valid != e.Valid || address != e.Address {
+			t.Errorf("Failed for \"%s\": wanted {%t, %s}, got {%t, %s}", e.Input, e.Valid, e.Address, valid, address)
+		}
+	}
+}
