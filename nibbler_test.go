ee9d038c3618897f33c5aff08819d310fc8506b8
Ryan McDonald <rjmcdonald83@gmail.com>
2015-06-15 10:52:35 -0600

    QRA-953: Add ı character to whitelist

--- nibbler_test.go
+++ nibbler_test.go
@@ -14,7 +14,7 @@ var examples = []example{
 	example{"hoot@hoot-hoot.com", true, "hoot@hoot-hoot.com"},
 	example{"woo/+@blah.com", true, "woo/+@blah.com"},
 	example{"#!$%&'*+-/=?^_`{}|~@example.org", true, "#!$%&'*+-/=?^_`{}|~@example.org"},
-	example{"üñîçøðé@üñîçøðé.com", true, "üñîçøðé@üñîçøðé.com"},
+	example{"üñîçøðé@üñîçøðéı.com", true, "üñîçøðé@üñîçøðéı.com"},
 	example{"\"Bob\" <bobthebuilder@dlc.com>", false, "\"Bob\""},
 }
 
